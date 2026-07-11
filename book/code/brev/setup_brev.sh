d8fd3ada5b13022bb026a6edef5a4ca935c1bbfe
Matthew Feickert <matthew.feickert@cern.ch>
2026-07-11 13:46:01 -0600

    fix: Install tools with Pixi global in Brev setup (#21)
    * Install useful additional tooling with Pixi global.
    * Use a direct path to the Pixi binary as sourcing ~/.bashrc seems to interrupt
      the Brev setup script.
    * Remove redundant sourcing.

--- book/code/brev/setup_brev.sh
+++ book/code/brev/setup_brev.sh
@@ -4,10 +4,12 @@
 curl -fsSL https://pixi.sh/install.sh | bash
 echo 'eval "$(pixi completion --shell bash)"' >> ${HOME}/.bashrc
 
+# Add extra tools that might be useful for the exercises
+# Need to use direct path to Pixi as sourcing ~/.bashrc causes interrupt
+$HOME/.pixi/bin/pixi global install bat curl git gh nvim rattler-build pixi-browse tree
+
 # Register a global "Python (Pixi)" Jupyter kernel for VS Code Remote.
 curl -sL https://raw.githubusercontent.com/matthewfeickert-talks/reproducible-cuda-workflows-with-pixi-scipy-2026/refs/heads/main/book/code/brev/setup_pixi_kernel.sh | bash
 
 # Clone the exercise repository
 git clone https://github.com/matthewfeickert-talks/reproducible-cuda-workflows-with-pixi-scipy-2026.git ${HOME}/reproducible-cuda-workflows-with-pixi-scipy-2026
-
-. ${HOME}/.bashrc
