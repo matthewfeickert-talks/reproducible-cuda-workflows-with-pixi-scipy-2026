bc0ddbd2f35659d2b22dc25ddc4c33586325d3f6
Ruben Arts <ruben.arts@hotmail.com>
2026-07-11 14:56:00 +0200

    Add clone of this repo to the setup script

--- book/code/brev/setup_brev.sh
+++ book/code/brev/setup_brev.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
+# Install Pixi
 curl -fsSL https://pixi.sh/install.sh | bash
 echo 'eval "$(pixi completion --shell bash)"' >> ${HOME}/.bashrc
 
+# Register a global "Python (Pixi)" Jupyter kernel for VS Code Remote.
 curl -sL https://raw.githubusercontent.com/matthewfeickert-talks/reproducible-cuda-workflows-with-pixi-scipy-2026/refs/heads/main/book/code/brev/setup_pixi_kernel.sh | bash
+
+# Clone the exercise repository
+git clone https://github.com/matthewfeickert-talks/reproducible-cuda-workflows-with-pixi-scipy-2026.git ${HOME}/reproducible-cuda-workflows-with-pixi-scipy-2026
 
 . ${HOME}/.bashrc
